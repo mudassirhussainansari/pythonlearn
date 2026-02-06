7a356d1961a8be6fd99c8e0510f3ad479010fe71
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-02-06 16:18:02 +0530

    introduction part

---
+++ Learn Python/introduction/expression.py
@@ -0,0 +1,12 @@
+first, second = -12,5
+third = first//second
+print(third)
+
+
+
+#this is integer division
+
+first_number = 12
+second_number = -5
+divide = second_number // first_number
+print(divide)
---
+++ Learn Python/introduction/second_datatype.py
@@ -0,0 +1,23 @@
+age = 29
+my_name = "Maddy"
+designation = "Imac lab assistant"
+salary = 12500
+print(type(my_name))
+print(type(age))
+print(type(designation))
+
+
+
+
+
+
+name="Mudassir"
+print(name*2*3)
+#if i concat string with int there are multiple string output
+
+
+
+
+a,b = "2",3
+text = "me"
+print((a+text)*b)
