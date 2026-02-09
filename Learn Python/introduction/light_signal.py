9f2381b38d210490cb33ba5f1bc8c3f10172147e
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-02-09 17:14:56 +0530

    light signal program

---
+++ Learn Python/introduction/light_signal.py
@@ -0,0 +1,9 @@
+light = input("Enter the light colour name: ").lower()
+if (light == "red"):
+    print("Stop")
+elif (light == "yellow"):
+    print("look")
+elif (light == "green"):
+    print("go")
+else:
+    print("Light is broken")
