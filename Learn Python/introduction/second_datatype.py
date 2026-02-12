6c467295233a7cb3d0b3c14b78c5c63d0cdba261
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-02-12 18:42:22 +0530

    refactor and remove space
    Refactor variable assignments and print statements for clarity.

--- Learn Python/introduction/second_datatype.py
+++ Learn Python/introduction/second_datatype.py
@@ -6,17 +6,9 @@ print(type(my_name))
 print(type(age))
 print(type(designation))
 
-
-
-
-
-
 name="Mudassir"
 print(name*2*3)
 #if i concat string with int there are multiple string output
-
-
-
 
 a,b = "2",3
 text = "me"
