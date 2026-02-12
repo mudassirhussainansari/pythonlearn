236d302a2e61df6a74b1959f8e2bec839da4789f
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-02-12 18:46:30 +0530

    Create Even_odd.py to check even or odd numbers
    This program used to check even and odd value

---
+++ Learn Python/introduction/Even_odd.py
@@ -0,0 +1,11 @@
+# wap to check the input user is even or odd
+
+value = int(input("Enter your number: "))
+
+if (value>0):
+    if (value%2==0):
+        print("Enter value is Even")
+    else:
+        print("Enter value is Odd")
+else:
+    print("Enter the valid value")
