c465515c5ba3bc4f74fe216f5b180af02d59e7df
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-02-12 18:32:38 +0530

    learn string with functions
    Add string manipulation functions to demonstrate various string methods in Python.

---
+++ Learn Python/introduction/string_with_function.py
@@ -0,0 +1,14 @@
+print(len(name))
+print(name[4])
+print(name.endswith("ir"))
+print(name.startswith("mu"))
+print(name.capitalize())
+print(name.title())
+fruits = "mango, banana, orange"
+print(fruits.split(","))
+fruits_list = fruits.split(",")
+print(fruits_list)
+print(("-").join(["mango", "banana"]))
+print(name.count("s"))
+string = "String"
+print(string.count("$"))
