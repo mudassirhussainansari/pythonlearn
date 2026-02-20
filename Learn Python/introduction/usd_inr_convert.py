2cac17c28e659ee5d8b17388c6225e0d242a298e
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-02-20 08:48:11 +0530

    USD to INR conversation program

---
+++ Learn Python/introduction/usd_inr_convert.py
@@ -0,0 +1,8 @@
+# convert usd to inr conversion
+
+usd_value = int(input("Enter the usd value: "))
+
+def usd_inr_conversion(usd_value):
+    return f"{usd_value * 91.05} INR"
+    
+print(usd_inr_conversion(usd_value))
