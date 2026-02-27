1d07d865abcd5ada2d412cb367f6f723a6fd8645
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-02-27 09:06:58 +0530

    Refactor password generation into a function

--- Learn Python/mini project/random_password_generator.py
+++ Learn Python/mini project/random_password_generator.py
@@ -1,34 +1,30 @@
 import random
 import string
 
+def password_generate(pass_len, choice):
+    generate_password = ""
+    if (choice == 1):
+        for i in range(pass_len):
+            generate_password += random.choice(string.ascii_uppercase)
+    elif (choice == 2):
+        for i in range(pass_len):
+            generate_password += random.choice(string.ascii_lowercase)
+    elif (choice == 3):
+        for i in range(pass_len):
+            generate_password += random.choice(string.digits)
+    elif (choice == 4):
+        for i in range(pass_len):
+            generate_password += random.choice(string.punctuation)
+    elif (choice == 5):
+        for i in range(pass_len):
+            generate_password += random.choice(string.ascii_letters + string.digits + string.punctuation)
+    else:
+        print("Please enter the valid choice like 1 to 4")
+    
+    return generate_password
+
 
 pass_len = int(input("Enter the password length: "))
-generate_password = ""
-
-upper_letter = string.ascii_uppercase
-lower_letter = string.ascii_lowercase
-digits = string.digits
-special_character = string.punctuation
-string_char = string.ascii_letters + string.digits + string.punctuation
-
 choice = int(input("please select you password is only \n 1. upper letter \n 2. lower letter \n 3. digit \n 4. special character \n 5. mix \n"))
 
-if (choice == 1):
-    for i in range(pass_len):
-        generate_password += random.choice(upper_letter)
-elif (choice == 2):
-    for i in range(pass_len):
-        generate_password += random.choice(lower_letter)
-elif (choice == 3):
-    for i in range(pass_len):
-        generate_password += random.choice(digits)
-elif (choice == 4):
-    for i in range(pass_len):
-        generate_password += random.choice(special_character)
-elif (choice == 5):
-    for i in range(pass_len):
-        generate_password += random.choice(string_char)
-else:
-    print("Please enter the valid choice like 1 to 4")
-    
-print("Your random password -->",generate_password)
+print("your password -->",password_generate(pass_len, choice))
