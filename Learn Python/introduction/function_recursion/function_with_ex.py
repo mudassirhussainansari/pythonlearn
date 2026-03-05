3d7d2507532ac6593d7aa84d571a2a28ce493cbc
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-03-05 09:01:08 +0530

    Create function_with_ex.py

---
+++ Learn Python/introduction/function_recursion/function_with_ex.py
@@ -0,0 +1,9 @@
+# function with example 
+
+
+def avg(first, second, third):
+    average = (first+ second + third) / 3
+    return average
+    
+total_avg = avg(int(input("Enter the first value: ")),int(input("Enter the first value: ")),int(input("Enter the first value: ")))
+print(f"Total average for given number : {total_avg}")
