74ff94b4f63a2af00a2e6eaeb1c3a5e8774b8e04
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-04-21 14:51:13 +0530

    positive and negative in python

---
+++ Learn Python/if_else_practical/positive_negative.py
@@ -0,0 +1,10 @@
+# Question 6: Positive, Negative, or Zero
+
+num = int(input("Enter the number: "))
+
+if num > 0:
+    print(num,"is positive")
+elif num == 0:
+    print(num,"is zero")
+else:
+    print(num,"is negative")
