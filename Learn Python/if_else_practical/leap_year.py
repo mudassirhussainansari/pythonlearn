014a61700c939bef18f4c734a55beaa19ed9b099
Mudassir Hussain Ansari <89689965+mudassirhussainansari@users.noreply.github.com>
2026-04-21 14:42:12 +0530

    leap year checker

---
+++ Learn Python/if_else_practical/leap_year.py
@@ -0,0 +1,8 @@
+# leap years
+
+input_year = int(input("Enter the year: "))
+
+if (input_year % 4 == 0 and input_year % 100 != 0) or (input_year % 400 == 0):
+    print(input_year, "is leap year")
+else:
+    print(input_year, "is not leap year")
